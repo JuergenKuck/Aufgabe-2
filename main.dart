64e38a87539bed620d9eef610b9d236a7a6e8d04
JuergenKuck <drj.kuck@googlemail.com>
2025-03-05 11:09:35 +0100

    final

--- main.dart
+++ main.dart
@@ -1,3 +1,26 @@
-void main(){
-    
+void main() {
+  Ausgabe("male", 20);
+}
+
+void Ausgabe(String sex, int age) {
+  switch (sex) {
+    case 'female':
+      switch (age) {
+        case >= 20 && < 25:
+          print("Im Schnitt 181,4m");
+        case >= 25 && < 30:
+          print("Im Schnitt 180,4m");
+        case >= 30 && < 35:
+          print("Im Schnitt 181,3m");
+      }
+    case 'male':
+      switch (age) {
+        case >= 20 && < 25:
+          print("Im Schnitt 167,5m");
+        case >= 25 && < 30:
+          print("Im Schnitt 167,3 m");
+        case >= 30 && < 35:
+          print("Im Schnitt 167,2");
+      }
+  }
 }
